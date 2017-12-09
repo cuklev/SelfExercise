d8301638cf50593b24e30a65f2fa14b5092b1635
Kristiyan Tsaklev <cuki@cukii.tk>
2017-12-09 18:52:42 +0200

    Some timsort implementation

---
+++ TimSort/timsort.hpp
@@ -0,0 +1,138 @@
+#pragma once
+
+#include <algorithm>
+#include <memory>
+#include <vector>
+
+// TODO: galloping
+template <typename It, typename Cmp>
+void merge(It begin, It middle, It end, Cmp cmp) {
+	using T = std::remove_reference_t<decltype(*begin)>;
+
+	// do less copying
+	begin = upper_bound(begin, middle, *middle, cmp);
+	end = lower_bound(middle, end, *(middle - 1), cmp);
+
+	const auto LSize = middle - begin;
+	const auto RSize = end - middle;
+
+	if(LSize <= RSize) { // left to right merging
+		auto buffer = std::make_unique<T[]>(LSize);
+		move(begin, middle, buffer.get());
+
+		auto i = buffer.get();
+		auto buffer_end = i + LSize;
+		auto j = middle;
+		auto k = begin;
+
+		while(j != end) {
+			if(cmp(*i, *j)) *k++ = std::move(*i++);
+			else *k++ = std::move(*j++);
+		}
+		move(i, buffer_end, k);
+	} else { // right to left merging
+		auto buffer = std::make_unique<T[]>(RSize);
+		move(middle, end, buffer.get());
+
+		auto i = middle - 1;
+		auto buffer_start = buffer.get();
+		auto j = buffer_start + RSize - 1;
+		auto k = end - 1;
+
+		while(i != begin - 1) {
+			if(cmp(*j, *i)) *k-- = std::move(*i--);
+			else *k-- = std::move(*j--);
+		}
+		move_backward(buffer_start, j + 1, k + 1);
+	}
+}
+
+inline size_t get_min_run_length(size_t n) {
+	while(n >= (1 << 6))
+		n = (n + 1) >> 1;
+	return n;
+}
+
+// is this correct?
+template <typename It, typename Cmp>
+void keep_invariant(std::vector<It>& runs, It end, Cmp cmp) {
+	while(runs.size() >= 3) {
+		auto x = runs.back();
+		auto y = runs[runs.size() - 2];
+		auto z = runs[runs.size() - 3];
+		auto x_len = end - x;
+		auto y_len = x - y;
+		auto z_len = y - z;
+
+		if(z_len <= y_len + x_len) {
+			//if(x_len < z_len) {
+//				merge(y, x, end, cmp);
+//				runs.pop_back();
+//			} else {
+				merge(z, y, x, cmp);
+				runs.erase(runs.end() - 2);
+//			}
+		} else if(y_len <= x_len) {
+				merge(y, x, end, cmp);
+				runs.pop_back();
+		} else {
+			break;
+		}
+	}
+}
+
+template <typename It, typename Cmp>
+void timsort(It begin, It end, Cmp cmp) {
+	const auto N = end - begin;
+	if(N < 2) return; // nothing to sort
+
+	const size_t MinRunLength = get_min_run_length(N);
+
+	std::vector<It> runs;
+
+	// scan for runs
+	for(auto it = begin; it != end;) {
+		auto run_begin = it;
+		runs.push_back(it);
+		if(end - it == 1) {
+			keep_invariant(runs, end, cmp);
+			break;
+		}
+
+		size_t run_length = 2;
+		bool decreasing = cmp(*(it + 1), *it);
+
+		it += 2;
+		while(it != end && cmp(*it, *(it - 1)) == decreasing) {
+			++it;
+			++run_length;
+		}
+
+		if(decreasing) reverse(run_begin, it);
+
+		while(it != end && run_length < MinRunLength) {
+			// binary insertion sort
+			auto tmp = std::move(*it);
+			auto pos = upper_bound(run_begin, it, tmp, cmp);
+			for(auto s_it = it; s_it != pos; --s_it)
+				*s_it = std::move(*(s_it - 1));
+			*pos = std::move(tmp);
+			++it;
+			++run_length;
+		}
+
+		keep_invariant(runs, it, cmp);
+	}
+
+	// is this correct?
+	// fold with merging
+	for(auto i = runs.size() - 1; i > 0; --i) {
+		merge(runs[i - 1], runs[i], end, cmp);
+	}
+}
+
+template <typename It>
+void timsort(It begin, It end) {
+	using T = std::remove_reference_t<decltype(*begin)>;
+	timsort(begin, end, std::less<T>());
+}
---
+++ TimSort/timsort_benchmark.cpp
@@ -0,0 +1,73 @@
+#include "timsort.hpp"
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <random>
+#include <vector>
+#include <chrono>
+#include <cassert>
+#include <iterator>
+#include <cstdlib>
+
+template <typename T>
+void time_test(std::vector<T> stuff, void (*sort_function)(typename std::vector<T>::iterator, typename std::vector<T>::iterator)) {
+	auto orig_stuff = stuff;
+	std::sort(orig_stuff.begin(), orig_stuff.end());
+
+	auto begin = std::chrono::system_clock::now();
+	sort_function(stuff.begin(), stuff.end());
+	auto end = std::chrono::system_clock::now();
+
+	std::chrono::duration<double> duration = end - begin;
+	std::cout << duration.count() << '\n';
+
+	assert(stuff == orig_stuff);
+}
+
+#define time_test(x, s) \
+	std::cout \
+		<< std::left \
+		<< std::setw(20) \
+		<< #s << ' '; \
+	time_test(x, s)
+
+int main() {
+	std::random_device rd;
+	std::mt19937_64 rng(rd());
+
+	for(auto N: {10, 100, 1000, 10000, 100000, 1000000}) {
+		std::cout << "N = " << N << '\n';
+		std::vector<decltype(rng())> numbers;
+
+		{
+			decltype(numbers)::value_type x = 0;
+			generate_n(back_inserter(numbers), N, [&x] {return ++x;});
+		}
+		shuffle(numbers.begin(), numbers.end(), rng);
+
+		time_test(numbers, timsort);
+
+		generate(numbers.begin(), numbers.end(), rng);
+
+		time_test(numbers, std::sort);
+		time_test(numbers, std::stable_sort);
+		time_test(numbers, timsort);
+
+		{ // qsort is ugly
+			auto begin = std::chrono::system_clock::now();
+			std::qsort(&numbers[0], numbers.size(),
+					sizeof(numbers[0]),
+					[] (const void* x, const void* y) -> int {
+						return *reinterpret_cast<const unsigned*>(x) < *reinterpret_cast<const unsigned*>(y);
+					});
+			auto end = std::chrono::system_clock::now();
+
+			std::chrono::duration<double> duration = end - begin;
+			std::cout
+				<< std::left
+				<< std::setw(20)
+				<< "qsort" << ' '
+				<< duration.count() << '\n';
+		}
+	}
+}
